464123dc5cd25f6b972b04481fb219f4cce814ca
MANIKSHARMA2000 <115614705+MANIKSHARMA2000@users.noreply.github.com>
2023-09-05 13:54:03 +0530

    text-elipsis

--- src/components/MessageCard.jsx
+++ src/components/MessageCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const MessageCard = ({ message }) => {
   return (
-    <div className="w-52 h-44 p-2 border-[1px] border-black shadow-lg m-3">
+    <div className="w-52 h-fit p-2 border-[1px] border-black shadow-lg m-3">
       <p>
         {" "}
         <span className="font-[800] ">From :</span>
@@ -12,7 +12,7 @@ const MessageCard = ({ message }) => {
         {" "}
         <span className="font-[800] ">To:</span> {message?.to}
       </p>
-      <p>
+      <p className=" text-ellipsis overflow-clip">
         {" "}
         <span className="font-[800] ">Message:</span> {message?.body}
       </p>
