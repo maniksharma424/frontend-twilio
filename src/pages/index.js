aa8e3e075ec8a4367ed52d33e45ba40348f639e2
MANIKSHARMA2000 <115614705+MANIKSHARMA2000@users.noreply.github.com>
2023-09-05 10:23:02 +0530

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -40,42 +40,49 @@ export default function Home() {
   };
 
   return (
-    <div className="w-full h-screen flex justify-center items-center">
-      <form
-        className="w-1/2 h-1/3 p-5 border-[1px] border-black flex flex-col justify-around items-start"
-        onSubmit={handleSubmit}
-      >
-        <label>
-          Phone Number:
-          <input
-            type="text"
-            name="phoneNumber"
-            value={phoneNumber}
-            onChange={(e) => setPhoneNumber(e.target.value)}
-            className="border-[1px] border-black w-full p-1 "
-          />
-        </label>
-        <br />
-        <label>
-          Message:
-          <textarea
-            type="text"
-            name="message"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            className="border-[1px] border-black w-full p-1 "
-          />
-        </label>
-        <br />
-        <div className="w-full flex justify-around">
-          <button className="px-5 border-[1px] border-black" type="submit">
-            Send Message
-          </button>
-          <Link href={"/Messages"}>
-            <button className="px-5 border-[1px] border-black" >See all messages</button>
-          </Link>
-        </div>
-      </form>
-    </div>
+    <>
+      <h1 className="text-[30px] font-[500]w w-full flex justify-center">
+        <p>Test whatsapp Messages</p>
+      </h1>
+      <div className="w-full h-screen flex justify-center items-center">
+        <form
+          className="w-1/2 h-1/3 p-5 border-[1px] border-black flex flex-col justify-around items-start"
+          onSubmit={handleSubmit}
+        >
+          <label>
+            Phone Number:
+            <input
+              type="text"
+              name="phoneNumber"
+              value={phoneNumber}
+              onChange={(e) => setPhoneNumber(e.target.value)}
+              className="border-[1px] border-black w-full p-1 "
+            />
+          </label>
+          <br />
+          <label>
+            Message:
+            <textarea
+              type="text"
+              name="message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              className="border-[1px] border-black w-full p-1 "
+            />
+          </label>
+          <br />
+          <div className="w-full flex justify-around">
+            <button className="px-5 border-[1px] border-black" type="submit">
+              Send Message
+            </button>
+            <Link href={"/Messages"}>
+              <button className="px-5 border-[1px] border-black">
+                See all messages
+              </button>
+            </Link>
+          </div>
+        </form>
+      </div>
+    </>
   );
 }
