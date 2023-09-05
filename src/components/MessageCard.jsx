b3727734eed1fa2c320863348a0921623a21eea8
MANIKSHARMA2000 <115614705+MANIKSHARMA2000@users.noreply.github.com>
2023-09-05 10:20:12 +0530

    initial commit

---
+++ src/components/MessageCard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const MessageCard = ({ message }) => {
+  return (
+    <div className="w-52 h-44 p-2 border-[1px] border-black shadow-lg">
+      <p>From : {message?.from}</p>
+      <p> To : {message?.to}</p>
+      <p> Message : {message?.body}</p>
+      <p> Status : {message?.status}</p>
+    </div>
+  );
+};
+
+export default MessageCard;
---
+++ src/pages/Messages.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect } from "react";
+import { useState } from "react";
+
+import MessageCard from "@/components/MessageCard";
+import Link from "next/link";
+const Messages = () => {
+  const [messages, setMessages] = useState([]);
+
+  const getMessageStatus = async () => {
+    await fetch("https://twillo-server.onrender.com/messages")
+      .then((res) => res.json())
+      .then((res) => setMessages(res));
+  };
+  useEffect(() => {
+    getMessageStatus();
+  }, []);
+  console.log(messages);
+  if (messages?.messages?.length > 0)
+    return (
+      <>
+        <div className="w-full flex flex-wrap justify-around h-[500px]">
+          {messages?.messages?.map((message) => {
+            return <MessageCard message={message} />;
+          })}
+        </div>
+        <button
+          className="px-5 border-[1px] border-black"
+          onClick={() => {
+            getMessageStatus();
+          }}
+        >
+          Refresh
+        </button>
+        <Link href={"/"}>
+          <button className="px-5 border-[1px] border-black">Go back</button>
+        </Link>
+      </>
+    );
+  else
+    return (
+      <Link href={"/"}>
+        <button>Start Sending Messages</button>
+      </Link>
+    );
+};
+
+export default Messages;
